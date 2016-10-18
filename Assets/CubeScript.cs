c3d572743526f8722993c2a023c32af9148a0c48
James Salemme <jsal2000@outlook.com>
2016-10-18 14:56:09 -0400

    I  added ____________

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate (new Vector3 (0,0,1)*Time.deltaTime*0.5f);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate (new Vector3 (0,0,-1)*Time.deltaTime*0.5f);
+		}if(Input.GetKey(KeyCode.A)){
+			transform.Translate (new Vector3 (-1,0,0)*Time.deltaTime*0.5f);
+		}if(Input.GetKey(KeyCode.D)){
+			transform.Translate (new Vector3 (1,0,0)*Time.deltaTime*0.5f);
+		}
+	}
+}
