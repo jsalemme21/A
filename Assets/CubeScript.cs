54045ac86e7edd87e8b95511957a512e759bca77
James Salemme <jsal2000@outlook.com>
2016-10-25 15:32:50 -0400

    I made it so i cant fly and my jump is fixed

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -5,8 +5,10 @@ public class CubeScript : MonoBehaviour {
 
 	private Rigidbody rbody;
 
+	public float moveSpeed;
+	public float jumpHeight;
 
-
+	bool onground = true;
 
 	// Use this for initialization
 	void Start () {
@@ -16,16 +18,33 @@ public class CubeScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey(KeyCode.W)){
-			transform.Translate (new Vector3 (0,0,1)*Time.deltaTime*0.5f);
+			transform.Translate (new Vector3 (0,0,1)*Time.deltaTime*moveSpeed, Space.World);
 		}
 		if(Input.GetKey(KeyCode.S)){
-			transform.Translate (new Vector3 (0,0,-1)*Time.deltaTime*0.5f);
+			transform.Translate (new Vector3 (0,0,-1)*Time.deltaTime*moveSpeed, Space.World);
 		}if(Input.GetKey(KeyCode.A)){
-			transform.Translate (new Vector3 (-1,0,0)*Time.deltaTime*0.5f);
+			transform.Translate (new Vector3 (-1,0,0)*Time.deltaTime*moveSpeed, Space.World);
 		}if(Input.GetKey(KeyCode.D)){
-			transform.Translate (new Vector3 (1,0,0)*Time.deltaTime*0.5f);
+			transform.Translate (new Vector3 (1,0,0)*Time.deltaTime*moveSpeed, Space.World);
 		}if (Input.GetKeyDown (KeyCode.Space)) {
-			rbody.velocity += transform.up * 10;	
+			if (onground) {
+				rbody.AddForce (Vector3.up * jumpHeight);	
+			}
+
+
 		}
 	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name.Equals("Plane")) {
+			onground = true;
+		}
+	}
+
+	void OnCollisionExit(Collision col) {
+		if (col.gameObject.name.Equals("Plane")) {
+			onground = false;
+		}
+	}
+
 }
