f132aa059daac540d045485d6eed038a086fb6ab
James Salemme <jsal2000@outlook.com>
2016-11-01 15:26:58 -0400

    Fixed camera

---
+++ Assets/FollowCube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCube : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void LateUpdate () {
+		transform.position = new Vector3 (target.transform.position.x, target.transform.position.y+5, target.transform.position.z-5);
+		transform.LookAt (target.transform);
+	}
+}
